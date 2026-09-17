181ec87a39e3429227a703a9aef80cdbbcd622db
unknown <kevin.audrich@gmail.com>
2026-09-17 19:18:50 +0700

    Update website

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function playMusic() {
 function checkName() {
   const input = document.getElementById('nameInput').value.trim();
   
-  if (input.toLowerCase() === 'kelly') {
+  if (input.toLowerCase() === 'yes') {
     playMusic();
     showScreen('screen-heart');
   } else {
